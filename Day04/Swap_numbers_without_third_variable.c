82dc87a9e3881b5dc29fd3b071279c7a9a65cb20
Tanishka36799 <TANISHKA.36799@stu.upes.ac.in>
2026-09-13 10:28:39 +0530

    Day 4 - Swapping numbers without using a third variable

--- Day04/Swap_numbers_without_third_variable.c
+++ Day04/Swap_numbers_without_third_variable.c
@@ -1,28 +1,28 @@
-//Q6: Write a program to swap two numbers using a third variable.
+//Q7: Write a program to swap two numbers without using a third variable.
 
 /*
 Sample Test Cases:
 Input 1:
-3 5
+10 20
 Output 1:
-After swap: 5 3
+After swap: 20 10
 
 Input 2:
--1 1
+7 14
 Output 2:
-After swap: 1 -1
+After swap: 14 7
 
 */
 
 #include <stdio.h>
 int main()
 {
-    int a,b,c=0;
+    int a,b;
     printf("Enter two numbers to be swapped: ");
     scanf("%d %d", &a, &b);
-    c=a;
-    a=b;
-    b=c;
+    a = a+b;
+    b = a-b;
+    a = a-b;
     printf("After swap: %d, %d", a, b);
     return 0;
 }
